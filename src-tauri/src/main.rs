d82483549a44f2f6d2459a6ebd2281027e7d1f32
jizey <1224500078@qq.com>
2025-12-20 19:05:54 +0800

    feat: add android build support and successfully generate debug apk

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,9 +2,5 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-  tauri::Builder::default()
-    .plugin(tauri_plugin_fs::init())
-    .plugin(tauri_plugin_dialog::init())
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+  heic_converter_lib::run();
 }
